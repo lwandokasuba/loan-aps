6e5a2f0c1b81da217f661cbca77d2a904769ffcd
lwandokasuba <lwandojapkas@gmail.com>
2025-06-04 18:53:52 +0200

    Add Swagger support and update application prefix in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new ConsoleLogger({
-      prefix: 'Loan System',
+      prefix: 'Loan Application System',
     }),
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Loan Application System')
+    .setDescription('The loan application system API description')
+    .setVersion('1.0')
+    .addTag('loan')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
