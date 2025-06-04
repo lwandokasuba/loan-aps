0e93ee6b3d5cdc30ab99b983e87bc1d1a737eadf
lwandokasuba <lwandojapkas@gmail.com>
2025-06-04 18:25:17 +0200

    Update TypeORM configuration in AppModule to use environment variables for database connection and include Client entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,19 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClientModule } from './client/client.module';
+import { Client } from './client/entities/client.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'password',
-      database: process.env.DB_NAME || 'mydatabase',
-      entities: [],
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: Number(process.env.DATABASE_PORT) || 5432,
+      username: process.env.DATABASE_USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'password',
+      database: process.env.DATABASE_NAME || 'mydatabase',
+      entities: [Client],
       synchronize: process.env.NODE_ENV !== 'production',
     }),
     ClientModule,
