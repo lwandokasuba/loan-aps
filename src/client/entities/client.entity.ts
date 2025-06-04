94fc5597805fba5e541fbd25d35afa00b541c275
lwandokasuba <lwandojapkas@gmail.com>
2025-06-04 18:01:08 +0200

    Implement Client entity with properties: id, name, national_id, and phone_number

--- src/client/entities/client.entity.ts
+++ src/client/entities/client.entity.ts
@@ -1,1 +1,16 @@
-export class Client {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('clients')
+export class Client {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  national_id: string;
+
+  @Column({ type: 'varchar', length: 15, unique: true })
+  phone_number: string;
+}
