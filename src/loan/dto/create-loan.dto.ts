68aed8d98afce152ee9f913dd2ecff4366ec914d
lwandokasuba <lwandojapkas@gmail.com>
2025-06-08 11:43:44 +0200

    Enhance CreateLoanDto with validation decorators for improved data integrity

--- src/loan/dto/create-loan.dto.ts
+++ src/loan/dto/create-loan.dto.ts
@@ -1,16 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ApiProperty } from '@nestjs/swagger';
 import { LoanStatus } from '../entities/loan.entity';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsUUID,
+  Max,
+  Min,
+} from 'class-validator';
 
 export class CreateLoanDto {
   @ApiProperty()
+  @IsNumber()
+  @Min(0)
+  @IsNotEmpty()
   amount: number;
 
   @ApiProperty()
+  @IsNumber()
+  @Min(0)
+  @Max(100)
+  @IsNotEmpty()
   interest_rate: number;
 
   @ApiProperty()
+  @IsEnum(LoanStatus)
+  @IsNotEmpty()
   status: LoanStatus;
 
   @ApiProperty()
+  @IsUUID()
+  @IsNotEmpty()
   client_id: string;
 }
