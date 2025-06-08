3ed67bfd7d75f388583d05e26d33fa92fa8eac00
lwandokasuba <lwandojapkas@gmail.com>
2025-06-08 11:44:10 +0200

    Refactor UpdateLoanDto by removing unnecessary ApiProperty decorator for cleaner code

--- src/loan/dto/update-loan.dto.ts
+++ src/loan/dto/update-loan.dto.ts
@@ -1,7 +1,4 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateLoanDto } from './create-loan.dto';
 
-export class UpdateLoanDto extends PartialType(CreateLoanDto) {
-  @ApiProperty()
-  id: string;
-}
+export class UpdateLoanDto extends PartialType(CreateLoanDto) {}
