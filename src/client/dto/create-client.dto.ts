4ef8070d15d369bfd33563827d5d44bd486720fb
lwandokasuba <lwandojapkas@gmail.com>
2025-06-08 11:34:50 +0200

    Enhance CreateClientDto with IsNotEmpty validation for improved data integrity

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,20 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ApiProperty } from '@nestjs/swagger';
-import { IsMobilePhone, IsString, Matches } from 'class-validator';
+import { IsMobilePhone, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CreateClientDto {
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @ApiProperty()
   @IsString()
+  @IsNotEmpty()
   @Matches(/^\d{6}\/\d{2}\/\d{1}$/, {
     message: 'National ID must be in the format XXXXXX/YY/Z',
   })
   national_id: string;
 
   @ApiProperty()
+  @IsNotEmpty()
   @IsMobilePhone('en-ZM')
   phone_number: string;
 }
