762e6847df557108ac9eb9723cf8b2354c0d19b7
lwandokasuba <lwandojapkas@gmail.com>
2025-06-08 11:24:04 +0200

    Refactor UpdateClientDto to remove unnecessary id property and related decorators

--- src/client/dto/update-client.dto.ts
+++ src/client/dto/update-client.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateClientDto } from './create-client.dto';
-import { ApiProperty } from '@nestjs/swagger';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-export class UpdateClientDto extends PartialType(CreateClientDto) {
-  @ApiProperty()
-  id: string;
-}
+export class UpdateClientDto extends PartialType(CreateClientDto) {}
