e2169312b651755801e5f97264cec0eecb98a48d
lwandokasuba <lwandojapkas@gmail.com>
2025-06-08 12:08:52 +0200

    Refactor CreateLoanDto to enforce minimum value for amount and interest_rate; enhance UpdateLoanDto with status property and validation

--- src/loan/dto/create-loan.dto.ts
+++ src/loan/dto/create-loan.dto.ts
@@ -1,33 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ApiProperty } from '@nestjs/swagger';
-import { LoanStatus } from '../entities/loan.entity';
-import {
-  IsEnum,
-  IsNotEmpty,
-  IsNumber,
-  IsUUID,
-  Max,
-  Min,
-} from 'class-validator';
+import { IsNotEmpty, IsNumber, IsUUID, Max, Min } from 'class-validator';
 
 export class CreateLoanDto {
   @ApiProperty()
   @IsNumber()
-  @Min(0)
+  @Min(1)
   @IsNotEmpty()
   amount: number;
 
   @ApiProperty()
   @IsNumber()
-  @Min(0)
+  @Min(1)
   @Max(100)
   @IsNotEmpty()
   interest_rate: number;
-
-  @ApiProperty()
-  @IsEnum(LoanStatus)
-  @IsNotEmpty()
-  status: LoanStatus;
 
   @ApiProperty()
   @IsUUID()
--- src/loan/dto/update-loan.dto.ts
+++ src/loan/dto/update-loan.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateLoanDto } from './create-loan.dto';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+import { LoanStatus } from '../entities/loan.entity';
 
-export class UpdateLoanDto extends PartialType(CreateLoanDto) {}
+export class UpdateLoanDto extends PartialType(CreateLoanDto) {
+  @ApiProperty()
+  @IsOptional()
+  @IsEnum(LoanStatus)
+  @IsNotEmpty()
+  status?: LoanStatus;
+}
