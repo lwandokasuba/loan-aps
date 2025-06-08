997418c0ac88d2984fbb2446a843b16f315c3d5b
lwandokasuba <lwandojapkas@gmail.com>
2025-06-08 09:27:23 +0200

    Enhance CreateClientDto with validation decorators for improved data integrity

--- src/client/dto/create-client.dto.ts
+++ src/client/dto/create-client.dto.ts
@@ -1,9 +1,20 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { ApiProperty } from '@nestjs/swagger';
+import { IsMobilePhone, IsString, Matches } from 'class-validator';
+
 export class CreateClientDto {
   @ApiProperty()
+  @IsString()
   name: string;
+
   @ApiProperty()
+  @IsString()
+  @Matches(/^\d{6}\/\d{2}\/\d{1}$/, {
+    message: 'National ID must be in the format XXXXXX/YY/Z',
+  })
   national_id: string;
+
   @ApiProperty()
+  @IsMobilePhone('en-ZM')
   phone_number: string;
 }
