cc85952c614b5197a1ac72811ee9f61840c00ff6
lwandokasuba <lwandojapkas@gmail.com>
2025-06-09 18:18:54 +0200

    Refactor ClientController tests: implement mock service and add client creation test

--- src/client/client.controller.spec.ts
+++ src/client/client.controller.spec.ts
@@ -1,11 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientController } from './client.controller';
 import { ClientService } from './client.service';
+import { CreateClientDto } from './dto/create-client.dto';
+
+const createClient: CreateClientDto = {
+  name: 'Lwando',
+  national_id: '123456/12/1',
+  phone_number: '0971234567',
+};
 
 describe('ClientController', () => {
   let controller: ClientController;
 
-  const mockClientService = {};
+  const mockClientService = {
+    create: jest.fn((dto: CreateClientDto) => {
+      return {
+        id: '1',
+        createdDate: new Date(),
+        updatedDate: new Date(),
+        ...dto,
+      };
+    }),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,5 +38,14 @@ describe('ClientController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a client', async () => {
+    expect(await controller.create(createClient)).toEqual({
+      id: expect.any(String),
+      ...createClient,
+      createdDate: expect.any(Date),
+      updatedDate: expect.any(Date),
+    });
   });
 });
